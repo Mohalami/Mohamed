b1c9cbb01c7384e9200e2207c3971db116b100b5
MOHA <mohalamindiaye@gmail.com>
2023-08-17 20:30:26 +0200

    Creation du portfolio

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Twig;
+
+use App\Repository\CategorieRepository;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    private function __construct(CategorieRepository $categorieRepository)
+    {
+        $this->categorieRepository= $categorieRepository;
+    }
+
+    public function getFunctions()
+    {
+        return[
+            new TwigFunction('categorieNavbar', [$this, 'categorie']),
+        ];
+    }
+
+    public function categorie(): array
+    {
+        return $this->categorieRepository->findAll();
+    }
+}
