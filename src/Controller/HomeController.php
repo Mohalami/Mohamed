cf9c2374099d73035ce3082e6df9c3be6199ff63
MOHA <mohalamindiaye@gmail.com>
2023-08-17 13:31:48 +0200

    mise a jour des lien navbar et footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function index(PeintureRepository $peintureRepository,
     BlogpostRepository $blogpostRepository): Response
     {
