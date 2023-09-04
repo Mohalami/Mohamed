5c0177cf924f8487e12ad9247487c2c165b81b4b
MOHA <mohalamindiaye@gmail.com>
2023-09-04 13:58:35 +0200

    creation du crud Blogpost

--- src/Controller/Admin/BlogpostCrudController.php
+++ src/Controller/Admin/BlogpostCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Blogpost;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class BlogpostCrudController extends AbstractCrudController
 {
@@ -12,14 +17,25 @@ class BlogpostCrudController extends AbstractCrudController
         return Blogpost::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('titre'),
+
+            // pour rendre le champ invisible dans le form on rejoute ->hideOnForm
+            TextField::new('slug')->hideOnForm(),
+            TextareaField::new('contenu'),
+            DateField::new('createdAt')->hideOnForm(),
         ];
     }
-    */
+
+    // Pour configurer le crud
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+        // Pour classer de facon decroissaante
+            ->setDefaultSort(['createdAt' => 'DESC']);
+    }
+    
 }
