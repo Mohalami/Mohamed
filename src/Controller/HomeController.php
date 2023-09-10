a6ac5ea5594d249c7918d7b9b10056daa3d74583
MOHA <mohalamindiaye@gmail.com>
2023-09-10 10:35:56 +0200

    Site sans fixtures

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\BlogpostRepository;
 use App\Repository\PeintureRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +13,14 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(PeintureRepository $peintureRepository,
-    BlogpostRepository $blogpostRepository): Response
+                          BlogpostRepository $blogpostRepository,
+                          UserRepository $userRepository
+                          ): Response
     {
         return $this->render('home/index.html.twig', [
             'peintures' => $peintureRepository->Lastree(),
             'blogposts' =>$blogpostRepository->Lastree(),
+            'peintre' => $userRepository->getPeintre()
         ]);
     }
 }
